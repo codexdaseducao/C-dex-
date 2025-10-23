46265cd3410c744040f4d7b6f6eccd5c0638fe7c
stark23471 <bg330137@gmail.com>
2025-10-23 12:38:14 -0300

    M

---
+++ projeto codex/schema.js
@@ -0,0 +1,17 @@
+{
+    "@context":"https://schema.org",
+    "@type":"Book",
+    "name":"Mente Desejante - Códex da Sedução Livro 1",
+    "author":{"@type":"Person","name":"Autor"},
+    "bookEdition":"Edição Alquímica",
+    "inLanguage":"pt-BR",
+    "description":"Descubra os segredos da atração. Transforme sua presença em um ímã irresistível de desejo e poder. Edição Alquímica do livro mais provocante sobre autodomínio e magnetismo.",
+    "image":"images/capa-codex-001.png",
+    "offers":{
+      "@type":"Offer",
+      "price":"47.90",
+      "priceCurrency":"BRL",
+      "url":"https://seu-checkout.com/mente-desejante",
+      "availability":"https://schema.org/InStock"
+    }
+  }
